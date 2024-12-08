c06e3fa08ed18d149dc1e7ffdaf8a8a6529ad36c
Lee jung woon <149851043+MotherBoardHal9000@users.noreply.github.com>
2024-12-08 11:08:14 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ let circleGraph = document.getElementById('circleGraph');
 let candleGraph = document.getElementById('candleGraph');
 
 
+window.onload = candleGraph.style.opacity = '1';
+window.onload = circleGraph.style.opacity = '1';
+window.onload = wave.style.opacity = '1';
+
 
 
 window.addEventListener("scroll",function(addEventListener){
     let value = window.scrollY;
     wave.style.transform = `translateY(${value * 0.85}px)`
-    wave.style.transform= "(45deg);"
     circleGraph.style.transform = `translateY(${value * 0.45}px)`
     candleGraph.style.transform = `translateY(${value * 0.25}px)`
     console.log("scroll");
+    window.requestAnimationFrame
     
 })
