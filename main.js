8f26d6bceca434cfe82b99d6ff16b89cbfaf1c64
Lee jung woon <149851043+MotherBoardHal9000@users.noreply.github.com>
2024-12-09 12:01:03 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ window.onload = wave.style.opacity = '1';
 
 window.addEventListener("scroll",function(addEventListener){
     let value = window.scrollY;
-    wave.style.transform = `translateY(${value * 0.85}px)`
-    circleGraph.style.transform = `translateY(${value * 0.45}px)`
+    wave.style.transform = `translateY(${value * 0.45}px)`
+    circleGraph.style.transform = `translateY(${value * 0.35}px)`
     candleGraph.style.transform = `translateY(${value * 0.25}px)`
     console.log("scroll");
     window.requestAnimationFrame
